41cf96c4d1b4e46823bd63912cceed9468b84df3
sidsitaula <siddharthasitaula02@gmail.com>
2022-07-21 17:16:44 +0545

    added mongo db connection

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //necessary imports
 const express = require("express");
 const env = require("dotenv");
+const mongoose = require("mongoose");
 
 //configuring env for environment variables
 env.config();
 
-
+//settingup mongodb atlas connection
+mongoose.connect(process.env.MONGO_URI, ()=>{
+    console.log("Connected to mongoDB atlas")
+})
 
 const app = express();
 app.listen(process.env.PORT_NUMBER ,()=>{
